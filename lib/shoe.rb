40f0929eba0ca1bb09e10bd2bbb2a7a663ef0b04
Luana Leal <luana.leal.eq@gmail.com>
2020-08-13 01:57:22 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,34 @@
 # Make your shoe class here!
+class Shoe 
+  
+  def initialize(brand) 
+    @brand =  brand 
+  end 
+  
+  def brand
+    @brand 
+  end 
+  
+  def color 
+    @color
+  end 
+  
+  def size 
+    @size
+  end 
+  
+  def material 
+    @material
+  end 
+  
+  def condition 
+    @condition
+  end 
+  
+  def 
+  
+  
+end 
+
+
+
